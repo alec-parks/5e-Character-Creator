ddb2e9f913b4b68f5dc1d28471424e1b9ce57516
Alexander Parks <alec.parks@gmail.com>
2015-03-13 12:11:36 -0500

    Update Attribute Add tests
    Update Attributes to Attribute. Add in method for calculating modifier.
    
    Started tests for Attribute class using xUnit unreleased 2.0 test
    package.

---
+++ Source/CharacterBuilder.Models.Tests/AttributeTests.cs
@@ -0,0 +1,25 @@
+﻿using CharacterBuilder.Models;
+using Xunit;
+
+namespace CharacterBuilder.Models.Tests
+{
+	public class AttributeTests
+	{
+		[Theory]
+		[InlineData(8,-1)]
+		[InlineData(7,-2)]
+		[InlineData(9,-1)]
+		[InlineData(10,0)]
+		[InlineData(11,0)]
+		[InlineData(12,1)]
+		public void ShouldComputeModifier(int score, int result)
+		{
+			var sut = new Attribute();
+
+			sut.Score = score;
+
+			Assert.Equal (result, sut.Modifier);
+		}
+	}
+}
+
---
+++ Source/CharacterBuilder/CharacterBuilder.Models/Attribute.cs
@@ -0,0 +1,28 @@
+﻿using Assisticant.Fields;
+using System;
+
+namespace CharacterBuilder.Models
+{
+	public class Attribute
+	{
+		private Observable<int> _score = new Observable<int>(8);
+
+		public int Score 
+		{
+			get { return _score.Value;}
+			set { _score.Value = value;}
+		}
+
+		public int Modifier 
+		{
+			get { return GetModifier(_score); }
+		}
+
+		private int GetModifier(int sc)
+		{
+			return (int) Math.Floor((sc - 10) / 2.0);
+		}
+
+	}
+}
+
