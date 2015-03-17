96b3c3b1bb5e098ce4f2dfb7b33bcff3945f2cb9
Alexander Parks <alec.parks@gmail.com>
2015-03-16 21:19:24 -0500

    More changes

--- Source/CharacterBuilder/CharacterBuilder.Models/DiceRoller.cs
+++ Source/CharacterBuilder/CharacterBuilder.Models/DiceRoller.cs
@@ -2,11 +2,26 @@
 
 namespace CharacterBuilder.Models
 {
-	public class DiceRoller
+	public class DiceRoller:IDice
 	{
-		public DiceRoller ()
+
+		readonly static Random DiceRoll = new Random();
+
+		public int Roll (int sides)
 		{
+			return DiceRoll.Next (1, sides + 1);
 		}
+
+		public int Roll(int dice, int sides)
+		{
+			int retValue = 0;
+			for (int i = 0; i < dice; i++)
+			{
+				retValue += DiceRoll.Next (1, sides + 1);
+			}
+			return retValue;
+		}
+
 	}
 }
 
--- Source/CharacterBuilder/CharacterBuilder.Models/IDice.cs
+++ Source/CharacterBuilder/CharacterBuilder.Models/IDice.cs
@@ -4,6 +4,7 @@ namespace CharacterBuilder.Models
 	public interface IDice
 	{
 		int Roll(int sides);
+		int Roll(int dice, int sides);
 	}
 }
 
